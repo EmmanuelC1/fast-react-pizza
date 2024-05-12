57a562cd8355bfc854dec2446873fd097554a8d2
emmanuelc1 <emmanuelcastillo1@gmail.com>
2024-05-11 21:14:23 -0700

    feat: placing new order using React Router Form component & actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Form, redirect } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = str =>
@@ -38,7 +40,9 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let&apos;s go!</h2>
 
-      <form>
+      {/* Form is a React Router form component */}
+      {/* <Form method='POST' action='/order/new'> */}
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -66,15 +70,35 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
+          {/* hidden input to also submit cart data coming from Redux */}
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+// once Form is submitted React Router will call the action function behind the scene (just like the loader function)
+// 'request' will be passed in to action func
+export async function action({ request }) {
+  const formData = await request.formData(); // formData() is a regular web API provided from the browser
+  const data = Object.fromEntries(formData); // turn formData into object
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on', // check if priority checkbox is checked (result will be true/false not on/off)
+  };
+
+  const newOrder = await createOrder(order);
+
+  // redirect function from React Router to navigate to new component/path. Can't use navigate as before because that is a hook and only can be used inside components and not functions
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
