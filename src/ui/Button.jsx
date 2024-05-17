36512eccb3f429995a173a2c76e66a8bce3332bf
emmanuelc1 <emmanuelcastillo1@gmail.com>
2024-05-16 20:31:30 -0700

    feat: user can add pizza to cart, using redux state

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
@@ -11,6 +11,7 @@ function Button({ children, disabled, to, type }) {
       'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-500 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:text-stone-800 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
   };
 
+  // if Button component has 'to' prop, render a Link component to navigate to path
   if (to) {
     return (
       <Link className={styles[type]} to={to}>
@@ -19,6 +20,16 @@ function Button({ children, disabled, to, type }) {
     );
   }
 
+  // if Button component has 'onClick' prop, render a button element with onClick method to handle onClick
+  if (onClick) {
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+
+  // simple button, only classname and disabled props
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
